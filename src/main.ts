607ee1bb7aeddbc1e0397ac7cf306d7941817b71
Igor Postolenko <ipostolenko@techmategroup.com>
2023-11-19 14:09:49 +0300

    add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import { envVariables } from './envVariables';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.enableCors({
+    // TODO: research
+    credentials: true,
+    origin: true,
+  });
+
   const port = envVariables.appPort;
   await app.listen(port);
   console.log('App is listening on port', port);
